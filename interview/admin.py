192534e62f2fa1d199e0f76b65c2c96cc72f5edb
“dangfulin” <2694977226@qq.com>
2021-11-26 17:30:10 +0800

    实现应聘者数据批量导出功能

--- interview/admin.py
+++ interview/admin.py
@@ -1,5 +1,18 @@
+import csv
+from datetime import datetime
+
 from django.contrib import admin
+from django.http import HttpResponse
+
 from interview.models import Candidate
+
+
+exportable_fields = ('username', 'city', 'phone',
+                     "degree", "bachelor_school",
+                     "first_result", "first_interviewer",
+                     "second_result", "second_interviewer",
+                     "hr_result", "hr_score","hr_remark","hr_interviewer"
+                     )
 
 
 # Register your models here.
@@ -28,3 +41,29 @@ class CandidateAdmin(admin.ModelAdmin):
         ('第二轮专业复试记录', {'fields': ("second_score", "second_learning_ability", "second_professional_competency", "second_pursue_of_excellence", "second_communication_ability", "second_pressure_score", "second_advantage", "second_disadvantage", "second_result", "second_recommend_position", "second_interviewer", "second_remark")}),
         ('HR复试记录', {'fields': ("hr_score", "hr_responsibility", "hr_communication_ability", "hr_logic_ability", "hr_potential", "hr_stability", "hr_advantage", "hr_disadvantage", "hr_result", "hr_interviewer", "hr_remark",)}),
     )
+
+    @admin.action(description='导出所选的应聘者信息到CSV文件')
+    def export_model_as_csv(self, request, queryset):
+        response = HttpResponse(content_type='text/csv')
+        field_list = exportable_fields
+        # 设置带 BOM 的编码
+        response.charset = 'utf-8-sig'
+        response['Content-Disposition'] = 'attachment; filename=recruitment-candidates_list-%s.csv' % (
+            datetime.now().strftime('%Y-%m-%d-%H-%M-%S'),
+        )
+        # 写入表头
+        writer = csv.writer(response)
+        writer.writerow(
+            [queryset.model._meta.get_field(f).verbose_name.title() for f in field_list]
+        )
+        # 写入数据
+        for obj in queryset:
+            # 单行记录
+            csv_line_values = []
+            for field in field_list:
+                field_object = queryset.model._meta.get_field(field)
+                field_value = field_object.value_from_object(obj)
+                csv_line_values.append(field_value)
+            writer.writerow(csv_line_values)
+
+        return response
