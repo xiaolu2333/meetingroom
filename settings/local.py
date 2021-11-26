091252eb6bcb64790d606aab2e351a28225c7371
“dangfulin” <2694977226@qq.com>
2021-11-26 17:41:17 +0800

    实现环境隔离

---
+++ settings/local.py
@@ -0,0 +1,11 @@
+from settings.base import *
+
+SECRET_KEY = 'django-insecure-naxa)ywnfh-p_1rx3psd*ca%j&tdpqrnjxkso&!5bh(fx1!gg7'
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['127.0.0.1', ]
+
+# LDAP_AUTH_URL = "ldap://server:389"
+# LDAP_AUTH_CONNECTION_USERNAME = admin
+# LDAP_AUTH_CONNECTION_PASSWORD = adminpassword
