951f756b6e6d78cbd8ebe8335bb4e1e68d9a0658
“dangfulin” <2694977226@qq.com>
2021-11-26 17:19:00 +0800

    集成LDAP认证

--- meetingroot/settings.py
+++ meetingroot/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_python3_ldap',
     'jobs',
     'interview',
 ]
@@ -120,3 +121,40 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# # LDAP
+# # The URL of the LDAP server.
+# LDAP_AUTH_URL = "ldap://localhost:389"
+# # Initiate TLS on connection.
+# LDAP_AUTH_USE_TLS = False
+#
+# # The LDAP search base for looking up users.
+# LDAP_AUTH_SEARCH_BASE = "dc=ihopeit,dc=com"
+# # The LDAP class that represents a user.
+# LDAP_AUTH_OBJECT_CLASS = "inetOrgPerson"
+#
+# # User model fields mapped to the LDAP
+# # attributes that represent them.
+# LDAP_AUTH_USER_FIELDS = {
+#     "username": "cn",
+#     "first_name": "givenName",
+#     "last_name": "sn",
+#     "email": "mail",
+# }
+#
+# # A tuple of django model fields used to uniquely identify a user.
+# LDAP_AUTH_USER_LOOKUP_FIELDS = ("username",)
+#
+# # Path to a callable that takes a dict of {model_field_name: value},
+# # returning a dict of clean model data.
+# # Use this to customize how data loaded from LDAP is saved to the User model.
+# LDAP_AUTH_CLEAN_USER_DATA = "django_python3_ldap.utils.clean_user_data"
+#
+# # The LDAP username and password of a user for querying the LDAP database for user
+# # details. If None, then the authenticated user will be used for querying, and
+# # the `ldap_sync_users` command will perform an anonymous query.
+# LDAP_AUTH_CONNECTION_USERNAME = None
+# LDAP_AUTH_CONNECTION_PASSWORD = None
+#
+# # LDAP login authentication
+# AUTHENTICATION_BACKENDS = {"django_python3_ldap.auth.LDAPBackend",'django.contrib.auth.backends.ModelBackend',}
