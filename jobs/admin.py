83ac591e7bd5bbf720610dfd2757a59d2031ddeb
“dangfulin” <2694977226@qq.com>
2021-11-27 15:39:21 +0800

    打通简历投递与面试流程

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,6 +1,9 @@
-from django.contrib import admin
+from datetime import datetime
+
+from django.contrib import admin, messages
 
 from jobs.models import Job, Resume
+from interview.models import Candidate
 
 
 # Register your models here.
@@ -16,6 +19,8 @@ class JobAdmin(admin.ModelAdmin):
 
 @admin.register(Resume)
 class ResumeAdmin(admin.ModelAdmin):
+    actions = ['enter_interview_process', ]
+
     list_display = ('username', 'applicant', 'city', 'apply_position', 'bachelor_school', 'master_school', 'major','created_date',)
 
     readonly_fields = ('applicant','created_date', 'modified_date',)
@@ -34,6 +39,20 @@ class ResumeAdmin(admin.ModelAdmin):
             ('created_date', 'modified_date'),
         )}),
     )
+
+    @admin.action(description='进入面试流程')
+    def enter_interview_process(self, request, queryset):
+        candidate_names = ""
+        for resume in queryset:
+            candidate = Candidate()
+            # 把简历信息添加到应聘者信息中
+            candidate.__dict__.update(resume.__dict__)
+            candidate.created_date = datetime.now()
+            candidate.modified_date = datetime.now()
+            candidate_names = candidate.username + ',' + candidate_names
+            candidate.creator = request.user.username
+            candidate.save()
+        messages.add_message(request, messages.INFO, '候选人 %s 已成功进入面试流程！' % candidate_names)
 
     def save_model(self, request, obj, form, change):
         obj.applicant = request.user
