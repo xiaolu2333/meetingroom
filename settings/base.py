79e58cbad3cf3da676ea5457164c58d5f1366bd3
xiaolu2333 <2694977226@qq.com>
2021-12-26 15:56:32 +0800

    feature: 集成sentry

--- settings/base.py
+++ settings/base.py
@@ -234,3 +234,20 @@ ACCOUNT_AUTHENTICATED_REGISTRATION_REDIRECTS = False
 SIMPLE_BACKEND_REDIRECT_URL = '/accounts/login/'
 # 登录成功后重定向
 LOGIN_REDIRECT_URL = '/'
+
+# Sentry
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+sentry_sdk.init(
+    dsn="http://b22e02de68c24cbaba2caa85393648c6@127.0.0.1:9000/2",
+    integrations=[DjangoIntegration()],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
